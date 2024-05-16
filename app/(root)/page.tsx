7897b47dce8f37e9b779cee1ab4415927b9aa0db
Jeel-vadher <jeelvadher0912@gmail.com>
2024-05-16 16:41:44 +0530

    implement intial file and folder structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
-const page = () => {
+const Home = () => {
+  const loggedIn = { firstName: "jeel" , lastName : "Vadher" ,email:"jeelvadher0912@gmail.com"};
+
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar  
+      user = {loggedIn}
+      transactions={[]}
+      banks={[{currentBalance:123.50},{currentBalance:1040.60}]}
+      />
+    </section>
+  );
+};
 
-export default page
+export default Home;
